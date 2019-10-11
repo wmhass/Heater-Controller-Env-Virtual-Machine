7b08f49c756aa889edcc242a1347762fd493e7bb
William Hass <wm.hass@gmail.com>
2019-10-11 18:22:55 -0300

    start service after build

--- github/repos/copy_docker_stack_repo.sh
+++ github/repos/copy_docker_stack_repo.sh
@@ -24,6 +24,7 @@ git clone --single-branch --branch $BRANCH --recurse-submodules $REPO_URL
 if [ -d $REPO_DESTINATION_DIRECTORY ]; then
     cd $REPO_DESTINATION_DIRECTORY
     sudo sh sh/updatestack.sh --all --build --pull
+    sudo sh sh/stack.sh --start --prod
 fi
 
 # TODO: Rebuild and recreate Docker Container
