a382eb9229b8005e891b578db967c0e7cc6a4632
William Hass <wm.hass@gmail.com>
2019-10-11 11:53:27 -0300

    reorganizing folders

---
+++ github/repos/copy_docker_stack_repo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+
+# Repository Destination Directory
+#REPO_DESTINATION_ROOT_DIRECTORY=/usr/local/var
+REPO_DESTINATION_ROOT_DIRECTORY=~/Desktop
+
+# Github Information
+GITHUB_DOMAIN=github.com
+GITHUB_USER=wmhass
+REPO_NAME=Heater-Controller-Server-Docker-Stack
+REPO_URL=git@$GITHUB_DOMAIN:$GITHUB_USER/$REPO_NAME.git
+BRANCH=master
+
+# Create repo root folder if does not exist
+if [ ! -d $REPO_DESTINATION_ROOT_DIRECTORY ]; then mkdir $REPO_DESTINATION_ROOT_DIRECTORY; fi
+
+# Delete repository directory if already exists
+REPO_DESTINATION_DIRECTORY=$REPO_DESTINATION_ROOT_DIRECTORY/$REPO_NAME
+if [ -d $REPO_DESTINATION_DIRECTORY ]; then sudo rm -rf $REPO_DESTINATION_DIRECTORY; fi
+
+# Clone repo
+cd $REPO_DESTINATION_ROOT_DIRECTORY
+git clone --single-branch --branch $BRANCH --recurse-submodules $REPO_URL
+
+if [ -d $REPO_DESTINATION_DIRECTORY ]; then
+    cd $REPO_DESTINATION_DIRECTORY
+    sh sh/buildprod.sh
+    sh sh/startprod.sh
+fi
+
+# TODO: Rebuild and recreate Docker Container
