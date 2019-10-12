5dc021df5139d5c7f9cf2815e3b4fc715df35bea
William Hass <wm.hass@gmail.com>
2019-10-12 11:10:40 -0300

    updating startup script

--- startup.sh
+++ startup.sh
@@ -4,13 +4,17 @@ echo Args: $ARGUMENTS
 sudo /usr/local/var/Heater-Controller-Server-Docker-Stack/sh/stack.sh --stop --prod
 sudo /usr/local/var/Heater-Controller-Server-Docker-Stack/sh/stack.sh --stop --dev
 
+# Run ENV='prod' vagrant up
 if [[ " ${ARGUMENTS[@]} " =~ "prod" ]]; then
   # Production
+  echo "!!!!!!!!! ====> Launching production stack, for production, do: ENV='prod' vagrant up"
   sudo /usr/local/var/Heater-Controller-Server-Docker-Stack/sh/updatestack.sh --all --pull --build
   sudo /usr/local/var/Heater-Controller-Server-Docker-Stack/sh/stack.sh --start --prod
 #elif [[ " ${ARGUMENTS[@]} " =~ " dev " ]]; then
+# Run ENV='dev' vagrant up
 else
   # Development
+  echo "====> ** Launching development stack"
   sudo /usr/local/var/Heater-Controller-Server-Docker-Stack/sh/stack.sh --start --dev
 fi
 
