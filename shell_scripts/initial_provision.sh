7487257985b7a1c4282573cee424081a425ef1b8
William Hass <wm.hass@gmail.com>
2019-10-16 08:54:06 -0300

    copying to vagrant user also

--- shell_scripts/initial_provision.sh
+++ shell_scripts/initial_provision.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
 
 # SSH Directory
-SSH_DIRECTORY=~/.ssh
+ROOT_SSH_DIRECTORY=/root/.ssh
+VAGRANT_SSH_DIRECTORY=/root/.ssh
 
 # Shared Directory
 VAGRANT_DIR=/vagrant
@@ -13,21 +14,28 @@ SSH_PRIVATE_KEY=$GITHUB_DIR/key/id_rsa
 SSH_PUBLIC_KEY=$GITHUB_DIR/key/id_rsa.pub
 
 # Create ssh Directory if it does not exist
-if [ ! -d $SSH_DIRECTORY ]; then mkdir -p $SSH_DIRECTORY; fi
-chmod 700 $SSH_DIRECTORY
+if [ ! -d $ROOT_SSH_DIRECTORY ]; then mkdir -p $ROOT_SSH_DIRECTORY; fi
+chmod 700 $ROOT_SSH_DIRECTORY
+
+if [ ! -d $VAGRANT_SSH_DIRECTORY ]; then mkdir -p $VAGRANT_SSH_DIRECTORY; fi
+chmod 700 $VAGRANT_SSH_DIRECTORY
 
 # Copy SSH Private Key
 if [ -f $SSH_PRIVATE_KEY ]; then 
-    cp $SSH_PRIVATE_KEY $SSH_DIRECTORY; 
+    cp $SSH_PRIVATE_KEY $ROOT_SSH_DIRECTORY;
+    cp $SSH_PRIVATE_KEY $VAGRANT_SSH_DIRECTORY;
 
     # Add Github as a known host
-    KNOWN_HOSTS=$SSH_DIRECTORY/known_hosts
-    ssh-keyscan github.com >> $KNOWN_HOSTS
+    ROOT_KNOWN_HOSTS=$ROOT_SSH_DIRECTORY/known_hosts
+    VAGRANT_KNOWN_HOSTS=$VAGRANT_SSH_DIRECTORY/known_hosts
+    ssh-keyscan github.com >> $ROOT_KNOWN_HOSTS
+    ssh-keyscan github.com >> $VAGRANT_KNOWN_HOSTS
 fi
 
 # Copy SSH Public Key
 if [ -f $SSH_PUBLIC_KEY ]; then 
-    cp $SSH_PUBLIC_KEY $SSH_DIRECTORY; 
+    cp $SSH_PUBLIC_KEY $ROOT_SSH_DIRECTORY; 
+    cp $SSH_PUBLIC_KEY $VAGRANT_SSH_DIRECTORY; 
 fi
 
 # Execute all scripts in SHARED_GITHUB_REPOS_DIR
